675a70e17ceceb6387473503ace9481cdb9d3b1f
Atharva Pradhan <41517998+AtharvaPradhan@users.noreply.github.com>
2019-03-03 11:49:29 +0530

    Update Running_Truck.C

--- Running_Truck.C
+++ Running_Truck.C
@@ -14,8 +14,8 @@ int main()
    setcolor(WHITE);
    line(0,midy+37,maxx,midy+37);
 
-   setcolor(12);
-   setfillstyle(SOLID_FILL,4);
+   setcolor(12);                                    \\ Light Red
+   setfillstyle(SOLID_FILL,4);                      \\ RED
    rectangle(0+i,midy+23,100+i,midy-10);
    floodfill(5+i,midy+22,12);
    rectangle(100+i,midy-50,140+i,midy+23);
@@ -24,15 +24,15 @@ int main()
    rectangle(110+i,midy-40,130+i,midy-20);
    floodfill(111+i,midy-38,12);
 
-   setcolor(0);
+   setcolor(0);                                     \\ Black
    setfillstyle(SOLID_FILL,0);
    arc(30+i,midy+23,0,180,11);
    arc(120+i,midy+23,0,180,11);
    floodfill(30+i,midy+23,0);
    floodfill(120+i,midy+23,0);
 
-   setcolor(7);
-   setfillstyle(SOLID_FILL,DARKGRAY);
+   setcolor(7);                                     \\ Light Grey
+   setfillstyle(SOLID_FILL,DARKGRAY);               \\ Color code 8
    circle(30+i,midy+25,10);
    circle(120+i,midy+25,10);
    floodfill(30+i,midy+25,7);
